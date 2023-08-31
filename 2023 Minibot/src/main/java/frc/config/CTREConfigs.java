917235ef6b219fa18a156024913a9187433866dd
Andrew-Willms <andrew.e.willms@gmail.com>
2023-08-30 23:39:14 -0400

    Added more items to config folder.

---
+++ 2023 Minibot/src/main/java/frc/config/CTREConfigs.java
@@ -0,0 +1,24 @@
+package frc.config;
+
+import com.ctre.phoenix.sensors.AbsoluteSensorRange;
+import com.ctre.phoenix.sensors.CANCoderConfiguration;
+import com.ctre.phoenix.sensors.SensorInitializationStrategy;
+import com.ctre.phoenix.sensors.SensorTimeBase;
+import frc.robot.Constants;
+
+
+
+public class CTREConfigs {
+
+	public CANCoderConfiguration swerveCanCoderConfig;
+
+	public CTREConfigs() {
+
+		swerveCanCoderConfig = new CANCoderConfiguration();
+		swerveCanCoderConfig.absoluteSensorRange = AbsoluteSensorRange.Unsigned_0_to_360;
+		swerveCanCoderConfig.sensorDirection = Constants.Swerve.CANCoderInvert;
+		swerveCanCoderConfig.initializationStrategy = SensorInitializationStrategy.BootToAbsolutePosition;
+		swerveCanCoderConfig.sensorTimeBase = SensorTimeBase.PerSecond;
+	}
+
+}
