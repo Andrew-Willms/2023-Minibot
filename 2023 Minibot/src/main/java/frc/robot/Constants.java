8ec32461f808a3b92befaa32e46ab12562274740
Andrew-Willms <andrew.e.willms@gmail.com>
2023-09-01 02:51:47 -0400

    removed unused import

--- 2023 Minibot/src/main/java/frc/robot/Constants.java
+++ 2023 Minibot/src/main/java/frc/robot/Constants.java
@@ -1,10 +1,5 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
